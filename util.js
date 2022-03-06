ca713b137d9b3501248c7d1128641666139ccd5c
mdanielpalumbo <71138682+mdanielpalumbo@users.noreply.github.com>
2022-03-05 23:04:02 -0300

    Delete util.js

--- util.js
+++
@@ -1,7 +0,0 @@
-import { Contenedor } from "./Contenedor.js";
-
-const prods = new Contenedor("productos")
-
-await prods.save({name:"Escuadra",price:"20",thumb:"##"})
-await prods.save({name:"Regla",price:"20",thumb:"##"})
-await prods.save({name:"Calculadora", price:"200", thumb:"##"})
